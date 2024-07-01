6549eda58abe7d177f116a35fc98dcbc28baf49e
ann-fox <fx8565@mail.ru>
2024-07-01 12:24:33 +0500

    fix: add SystemStatus

--- tablet/StatusBar.vue
+++ tablet/StatusBar.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="status-bar">
-    <button v-if="page == 'TaskList'" class="status-bar__button" v-touch-hold:2000.mouse="handleHold"
-            @mousedown="startAnimation" @touchstart="startAnimation" @mouseup="finishAnimation"
-            @touchend="finishAnimation">
-      <img src="../../assets/images/tablet-icons/logout.svg" alt="Выход">{{ $t('logout') }}
-    </button>
-    <button v-else class="status-bar__button" @click="goBack"><img src="../../assets/images/tablet-icons/arrowBack.svg">{{ $t('back') }}
-    </button>
-    <p class="status-bar__task">{{ textTask }}</p>
-<!--    <PrinterStatus :color="printer.status" :statusType="printer.text"/>-->
+    <button v-if="route.name == 'TaskList'" class="status-bar__button" v-touch-hold:2000.mouse="handleHold"
+            @mousedown="startAnimation" @touchstart="startAnimation" @mouseup="finishAnimation" @touchend="finishAnimation">
+      <img src="../images/logout.svg">{{ $t('logout') }}</button>
+    <button v-else class="status-bar__button" @click="goBack"><img src="../images/arrowBack.svg">{{ $t('back') }}</button>
+    <p class="status-bar__task">{{ otkStore.taskName }}</p>
     <div class="row">
-      <button v-if="page == 'TaskList'" class="status-bar__button new-task mr-30" @click="scanTaskCode">{{ $t('newJob') }}</button>
-      <PrinterStatus :color="printer.status" :statusType="printer.text" />
+      <button v-if="route.name == 'TaskList'" class="status-bar__button bg-primary mr-30" @click="scanTaskCode">{{ $t('newJob') }}</button>
+      <SystemStatus :color="systemStatus.status" :statusType="systemStatus.text" />
     </div>
   </div>
 </template>
 
 <script setup>
 // import axios from 'axios'
-import PrinterStatus from '../PrinterStatus.vue'
+import SystemStatus from "./SystemStatus.vue";
 import {ref, reactive, onMounted} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
 // import { useNewOtkStore } from '../stores/newOtkStore'
@@ -28,11 +24,32 @@ const router = useRouter()
 // const otkStore = useNewOtkStore()
 
 const isLogoutSubmitted = ref(false)
+// let requestTimer
 
-const printer = reactive({
-  status: 'ok',
-  text: 'READY'
+const systemStatus = reactive({
+  // status: 'error',
+  // text: 'NOT_READY'
+  // status: 'ok',
+  // text: 'READY'
+  // status: 'ok',
+  // text: 'DISCONNECTED'
 })
+// async function getSystemStatus () {
+//   const listDevices = [{"status": "Normal","ok": true},{"status": "Normal","ok": true},{"status": "Normal","ok": true}]
+//
+//   if (listDevices.length === 0 || listDevices.some(item => { return item.ok === false })) { // проверка на пустой массив объектов или значение каждого item.ok = false
+//     systemStatus.text = "NOT_READY"
+//     systemStatus.status = "error" //красный индикатор
+//   } else {
+//     systemStatus.text = "READY"
+//     systemStatus.status = "ok"
+//   }
+// }
+
+// const printer = reactive({
+//   status: 'ok',
+//   text: 'READY'
+// })
 
 const props = defineProps({
   // title: {
@@ -50,18 +67,8 @@ const props = defineProps({
 })
 
 // onMounted(async () => {
-//   const options = {
-//     method: 'GET',
-//     url: 'https://stoplight.io/mocks/anylabel/labeling-api/11880226/v1/system/status',
-//     headers: {Accept: 'application/json'}
-//   };
-//
-//   try {
-//     const { data } = await axios.request(options);
-//     console.log(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
+//   await getSystemStatus()
+//   requestTimer = setInterval(getSystemStatus, otkStore.refresh)
 // })
 
 
@@ -185,11 +192,5 @@ function finishAnimation(event) {
   100% {
     width: 100%;
   }
-}
-
-.new-task {
-  background: $primary;
-  font-family: 'Golos UI';
-  //letter-spacing: -0.24px;
 }
 </style>
