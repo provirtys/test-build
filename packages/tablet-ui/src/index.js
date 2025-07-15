477e33190018e213be15fd6bca69ea011689e9e8
Ssv <ssv@anylabel.dev>
2025-07-15 15:47:27 +0500

    fix(export): change export path DEV-203

--- packages/tablet-ui/src/index.js
+++ packages/tablet-ui/src/index.js
@@ -1,4 +1,4 @@
-export { VButton } from '@base';
+export { VButton } from '@integrity/base-ui/src';
 export { TaskCard } from '@components/Blocks/TaskCard';
 export { VCheckboxButton } from '@components/ui/VCheckboxButton';
 export { StatusIndicator } from '@components/Elements/StatusIndicator';
