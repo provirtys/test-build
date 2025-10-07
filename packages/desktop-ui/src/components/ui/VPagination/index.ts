2e3c4b1c2b84001b79f3c1dcd82c9e1655517f4f
Ssv <ssv@anylabel.dev>
2025-10-07 10:23:16 +0500

    chore: add PaginationNavigateData type to export DEV-304

--- packages/desktop-ui/src/components/ui/VPagination/index.ts
+++ packages/desktop-ui/src/components/ui/VPagination/index.ts
@@ -1,2 +1,2 @@
-export type { VPaginationModel } from './VPagination.types';
+export type { VPaginationModel, VPaginationNavigateData } from './VPagination.types';
 export { default as VPagination } from './VPagination.vue';
