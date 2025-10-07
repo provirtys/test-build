4ca84fe4d887bf12cb1cc91dbbfb8b0a1cfa3589
Ssv <ssv@anylabel.dev>
2025-10-07 10:12:58 +0500

    chore: add StickerTemplateItem type to export DEV-304

--- packages/desktop-ui/src/components/Blocks/StickerTemplateForm/index.ts
+++ packages/desktop-ui/src/components/Blocks/StickerTemplateForm/index.ts
@@ -1,2 +1,2 @@
-export type { StickerTemplateFormProps } from './StickerTemplateForm.types';
+export type { StickerTemplateFormProps, StickerTemplateItem } from './StickerTemplateForm.types';
 export { default as StickerTemplateForm } from './StickerTemplateForm.vue';
