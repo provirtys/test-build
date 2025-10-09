50da9f304edba6e8d836f2d722e82093982b66dd
Ssv <ssv@anylabel.dev>
2025-10-09 14:37:39 +0500

    chore: update exports in VTable component DEV-304

--- packages/desktop-ui/src/components/ui/VTable/index.ts
+++ packages/desktop-ui/src/components/ui/VTable/index.ts
@@ -1,2 +1,2 @@
-export type { VTableColumn, VTableProps } from './VTable.types';
+export type { VTableColumn, VTableEmitsRequest, VTableProps } from './VTable.types';
 export { default as VTable } from './VTable.vue';
