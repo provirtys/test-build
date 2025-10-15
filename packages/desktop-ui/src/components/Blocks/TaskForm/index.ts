87a0deadcb5cceb07ccf91decacfa63abf592477
Ssv <ssv@anylabel.dev>
2025-10-15 17:09:17 +0500

    feat: add task form's loading states to exports DEV-304

--- packages/desktop-ui/src/components/Blocks/TaskForm/index.ts
+++ packages/desktop-ui/src/components/Blocks/TaskForm/index.ts
@@ -1,2 +1,2 @@
-export type { SelectFieldOptions, TaskFormEmits, TaskFormProps } from './TaskForm.types';
+export type { SelectFieldOptions, TaskFormEmits, TaskFormLoadingStates, TaskFormProps } from './TaskForm.types';
 export { default as TaskForm } from './TaskForm.vue';
