03cd34b964310d7f199b5f7e6ab071e264b6d783
provirtys <75607451+provirtys@users.noreply.github.com>
2025-12-02 09:02:15 +0000

    Deploying to gh-pages from @ provirtys/test-build@80524a6d56cbb484daefba7be17bf03c2cfbc6bc 🚀

---
+++ assets/README.stories-BY1ljmOr.js
@@ -0,0 +1,3 @@
+const s={title:"Root/Overview"},e=()=>({template:"<div><h1>Root Storybook - используйте Refs слева</h1></div>"});var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`() => ({
+  template: '<div><h1>Root Storybook - используйте Refs слева</h1></div>'
+})`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const a=["Welcome"];export{e as Welcome,a as __namedExportsOrder,s as default};
