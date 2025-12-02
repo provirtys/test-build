18f3bd40e540610534a0b97e5a4a14a3e7d12c7d
Ssv <ssv@anylabel.dev>
2025-12-02 12:42:55 +0500

    build: add refs to other storybooks DEV-000

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  stories: [],
+  addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-a11y'],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'vite.config.js',
+      },
+    },
+  },
+  refs: {
+    baseui: {
+      title: 'Base UI',
+      url: 'https://https://provirtys.github.io/test-build/base-ui/iframe.html',
+    },
+    desktop: {
+      title: 'Desktop',
+      url: 'https://https://provirtys.github.io/test-build/desktop/iframe.html',
+    },
+    mobile: {
+      title: 'Mobile',
+      url: 'https://https://provirtys.github.io/test-build/mobile/iframe.html',
+    },
+    tablet: {
+      title: 'Tablet',
+      url: 'https://https://provirtys.github.io/test-build/tablet/iframe.html',
+    },
+  },
+};
+
+export default config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
